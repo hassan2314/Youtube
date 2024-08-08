14c1f29c337b74dd0f0fcfec926e6bb96f0a4d0e
hassan2314 <hassan87691@gmail.com>
2024-08-08 13:56:48 +0500

    All setup confrig

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//import rotuers
 
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
 
 export { app };
