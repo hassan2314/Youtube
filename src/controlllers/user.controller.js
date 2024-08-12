054bbb93517201c265712c1f8e31d90a1ba36e6a
hassan2314 <hassan87691@gmail.com>
2024-08-12 23:58:14 +0500

    Login User Complete

--- src/controlllers/user.controller.js
+++ src/controlllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const genrateAccessTokenAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findOne(userId);
+    const accessToken = user.genrateAccessToken();
+    const refreshToken = user.genrateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(502, "Server Error");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   try {
     // Get user details from the request body
@@ -82,4 +97,55 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  //Input password and email/username
+  //check username / email
+  //find the user
+  //password check
+  //access and referesh token
+  //send cookie
+
+  const { email, username, password } = req.body;
+  if (!email || !username) {
+    throw new ApiError(401, "Email or Username is Requried");
+  }
+  const user = await User.findOne({ $or: [{ email }, { username }] });
+  if (!user) {
+    throw new ApiError(404, "User Not Found");
+  }
+
+  const isPaswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPaswordValid) {
+    throw new ApiError(401, "Password is incorrect");
+  }
+
+  const { accessToken, refreshToken } = await genrateAccessTokenAndRefreshToken(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+export { registerUser, loginUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,7 +62,7 @@ userSchema.method.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.method.genrateAccesToken = function () {
+userSchema.method.genrateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
