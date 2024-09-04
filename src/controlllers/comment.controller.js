78623820647e600da7e6056d7aead88b680c7bd9
hassan2314 <hassan87691@gmail.com>
2024-09-04 14:43:52 +0500

    Add PipeLine in Comment

--- src/controlllers/comment.controller.js
+++ src/controlllers/comment.controller.js
@@ -2,19 +2,66 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Comment } from "../models/comment.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import mongoose from 'mongoose';
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
 
-})
+  const comment = await Comment.aggregate([
+    [
+      {
+        $match: {
+          video: new mongoose.Types.ObjectId(videoId)
+          
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner_details",
+        },
+      },
+      {
+        $addFields: {
+          owner_details: {
+            $first: "$owner_details",
+          },
+        },
+      },
+      {
+        $addFields: {
+          owner_name: "$owner_details.fullname",
+        },
+      },
+      {
+        $limit: limit
+      },
+      {
+        $project: {
+          owner_details: 0,
+          video : 0,
+          owner : 0,
+          createdAt : 0,
+          updatedAt : 0,
+          __v : 0
+        },
+      },
+    ]
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, comment[0], "Comment Fetched"))
+});
 
 const addComment = asyncHandler(async (req, res) => {
-  const {content} = req.body;
-  console.log(content)
+  const { content } = req.body;
+  console.log(content);
   const owner = req.user;
   const { videoId } = req.params;
 
@@ -26,7 +73,7 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(404, "User Not Found");
   }
 
-  if (typeof content !== 'string' || !content.trim()) {
+  if (typeof content !== "string" || !content.trim()) {
     throw new ApiError(400, "Invalid Comment Content");
   }
 
@@ -36,7 +83,7 @@ const addComment = asyncHandler(async (req, res) => {
     video: videoId,
   });
   if (!comment) {
-    throw new ApiError(501,"Comment error")
+    throw new ApiError(501, "Comment error");
   }
 
   const commentCreated = await Comment.findOne({ _id: comment._id })
@@ -49,13 +96,13 @@ const addComment = asyncHandler(async (req, res) => {
 });
 
 const updateComment = asyncHandler(async (req, res) => {
-  console.log(req.body)
-  const {updatedComment} = req.body;
-  const {commentId} = req.params;
+  console.log(req.body);
+  const { updatedComment } = req.body;
+  const { commentId } = req.params;
 
-  console.log(updatedComment)
+  console.log(updatedComment);
 
-  if (typeof updatedComment !== 'string' || !updatedComment.trim()) {
+  if (typeof updatedComment !== "string" || !updatedComment.trim()) {
     throw new ApiError(400, "Invalid Comment Content");
   }
 
@@ -66,42 +113,32 @@ const updateComment = asyncHandler(async (req, res) => {
   const updated = await Comment.findByIdAndUpdate(
     commentId,
     {
-      $set: {content: updatedComment.trim()},
+      $set: { content: updatedComment.trim() },
     },
     {
       new: true,
     }
   );
 
-  if(!updated){
+  if (!updated) {
     throw new ApiError(404, "Comment not found in DB");
   }
-  return res
-  .status(200)
-  .json(new ApiResponse(200,updated,"Message Updated"))
+  return res.status(200).json(new ApiResponse(200, updated, "Message Updated"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
-
-  const {commentId} = req.params;
+  const { commentId } = req.params;
 
   if (!commentId) {
     throw new ApiError(404, "Comment not found");
   }
 
-  const deleted = await Comment.findByIdAndDelete(
-    commentId
-  );
+  const deleted = await Comment.findByIdAndDelete(commentId);
 
-  if(!deleted){
+  if (!deleted) {
     throw new ApiError(404, "Comment not found in DB");
   }
-  return res
-  .status(200)
-  .json(new ApiResponse(200,deleted,"Message Deleted"))
+  return res.status(200).json(new ApiResponse(200, deleted, "Message Deleted"));
 });
-
-
-
 
 export { addComment, updateComment, deleteComment, getVideoComments };
